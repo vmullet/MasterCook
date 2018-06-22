b1b7c7807a586ec59c235f044b726abd9949f77d
Valentin <valentin.mullet@gmail.com>
2018-06-22 21:29:49 +0200

    Add CookerProfile and integrate into admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+
+from .models import CookerProfile
+from .models import Country
 
 
 # Register your models here.
+
+class ProfileInLine(admin.StackedInline):
+    model = CookerProfile
+    can_delete = False
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = [ProfileInLine]
+
+
+admin.site.unregister(User)
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Country)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Country(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class CookerProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    date_of_birth = models.DateField(null=True, blank=True)
+    bio = models.TextField(max_length=1000, blank=True)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE, null=True)
+
+
+

d0e603993f42e151b49cd7f808c79c75ba24e9c7
Valentin <valentin.mullet@gmail.com>
2018-06-22 21:51:45 +0200

    Added avatar image field to CookerProfile

--- accounts/models.py
+++ accounts/models.py
@@ -15,6 +15,7 @@ class CookerProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     date_of_birth = models.DateField(null=True, blank=True)
     bio = models.TextField(max_length=1000, blank=True)
+    avatar = models.ImageField(default='default_avatar.png', blank=True)
     country = models.ForeignKey(Country, on_delete=models.CASCADE, null=True)
 
 

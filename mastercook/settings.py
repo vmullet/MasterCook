478f44a4f414cef5eaff43783e48c66d7508a4b1
Valentin <valentin.mullet@gmail.com>
2018-06-23 19:08:11 +0200

    Internationalized templates

--- mastercook/settings.py
+++ mastercook/settings.py
@@ -108,7 +108,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr'
 
 TIME_ZONE = 'UTC'
 

6c3760c84131b5a576c46ff252fb728165afa2a1
Valentin <valentin.mullet@gmail.com>
2018-06-23 17:14:36 +0200

    Added comment, rate and cost into RecipeModel

--- recipes/models.py
+++ recipes/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from generic.models import Country, UnitMeasure
+from generic.models import Country, UnitMeasure, Currency
 from ingredients.models import Ingredient
 
 
@@ -30,17 +30,32 @@ class RecipeSkill(models.Model):
         return '%d - %d'.format(self.name, self.value)
 
 
+class RecipeCost(models.Model):
+    cost = models.FloatField
+    currency = models.ForeignKey(Currency, on_delete=models.CASCADE)
+
+
 class Recipe(models.Model):
     """
     Model to represent a recipe
     """
     name = models.CharField(max_length=100)
+    slug = models.SlugField
     description = models.TextField
     thumbnail = models.ImageField(default='default.png', blank=True)
+    preparation_time = models.DurationField
+    cooking_time = models.DurationField
+    cooling_time = models.DurationField(default=-1)
+    median_rate = models.FloatField(default=-1)
+    # Foreign Keys
     recipe_type = models.ForeignKey(RecipeType, on_delete=models.CASCADE)
     recipe_skill = models.ForeignKey(RecipeSkill, on_delete=models.CASCADE)
+    recipe_cost = models.OneToOneField(RecipeCost, on_delete=models.CASCADE)
     recipe_origin = models.ForeignKey(Country, on_delete=models.CASCADE)
+    # MetaData
     author = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return '%s - %s by %s'.format(self.name,
@@ -86,3 +101,25 @@ class RecipeImage(models.Model):
 
     def __str__(self):
         return '%s (%s) from %s'.format(self.name, self.image.url, self.recipe.name)
+
+
+class RecipeComment(models.Model):
+    """
+    Model to represent a comment written by a user for a recipe
+    """
+    comment = models.TextField
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+
+
+class RecipeRate(models.Model):
+    """
+    Model to represent a rate done by a user to a recipe
+    """
+    rate = models.IntegerField
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)

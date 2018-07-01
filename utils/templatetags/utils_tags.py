f0e46ba6dabb764c2c397689997b63dbaafc5775
LostArchives <valentin.mullet@gmail.com>
2018-07-01 20:05:29 +0200

    Refactored base template and footer

--- utils/templatetags/utils_tags.py
+++ utils/templatetags/utils_tags.py
@@ -7,3 +7,13 @@ register = template.Library()
 @register.inclusion_tag('utils/_browse_menu.html')
 def advanced_search_menu():
     return {'types': RecipeType.objects.all(), 'skills': RecipeSkill.objects.all()}
+
+
+@register.inclusion_tag('utils/_footer_menu_type.html')
+def footer_category_menu():
+    return {'types': RecipeType.objects.all()}
+
+
+@register.inclusion_tag('utils/_footer_menu_skill.html')
+def footer_skill_menu():
+    return {'skills': RecipeSkill.objects.all()}
